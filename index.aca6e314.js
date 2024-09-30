1c303cb96c4e5c9bebe68b1d680fc0515b7ba437
Yana Myshkivska <ymyshkivska@gmail.com>
2024-09-30 22:43:36 +0300

    make build

---
+++ index.aca6e314.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var t=document.querySelector(".wall"),e=document.querySelector(".spider"),n=t.getBoundingClientRect(),o=n.width,i=n.height,c=e.getBoundingClientRect(),d=c.width,l=c.height;Object.assign(e.style,{position:"absolute",left:"".concat((o-d)/2,"px"),top:"".concat((i-l)/2,"px")})});
+//# sourceMappingURL=index.aca6e314.js.map
